80ffabdb1ef976b67b89f93cd5f4e8667042a806
luwm <flappy8023@163.com>
2020-09-01 14:25:27 +0800

    021:合并两个有序链表-递归实现

--- src/easy/_021合并两个有序链表/Solution.java
+++ src/easy/_021合并两个有序链表/Solution.java
@@ -11,6 +11,11 @@ package easy._021合并两个有序链表;
  * }
  */
 class Solution {
+    /**迭代
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode result = new ListNode();
         ListNode cursor = result;
@@ -33,6 +38,25 @@ class Solution {
         }
         return result.next;
 
+    }
+
+    /**递归
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists1(ListNode l1,ListNode l2){
+        if(l1==null){
+            return l2;
+        }else if(l2==null){
+            return l1;
+        }else if(l1.val>=l2.val){
+            l2.next=mergeTwoLists1(l1,l2.next);
+            return l2;
+        }else {
+            l1.next = mergeTwoLists1(l1.next,l2);
+            return l1;
+        }
     }
 
     public static class ListNode {
