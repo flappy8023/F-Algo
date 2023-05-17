1a3578da05a01aa1e14ce239c2b5e790e7a8f45b
flappy8023 <flappy8023@163.com>
2023-05-17 11:22:28 +0800

    poj 2251 replace LinkedList with Array

--- src/poj/p2251/Main.java
+++ src/poj/p2251/Main.java
@@ -1,120 +1,104 @@
 package poj.p2251;
 
-import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
  * @author luweiming
  * @version 1.0.0
- * @ClassName Main.java
- * @Description 3D迷宫 http://poj.org/problem?id=2251
- * @createTime 2023年05月10日 18:12:00
+ * @ClassName Main1.java
+ * @Description TODO
+ * @createTime 2023年05月17日 10:44:00
  */
 public class Main {
-    private static char[][][] dungeon ;
-    private static boolean[][][] vis ;
-    private static int[][] dirs = {{1, 0, 0}, {-1, 0, 0}, {0, 1, 0},
-            {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
-    private static MyPoint start, end;
-    private static int result = -1;
+    static int[][] moves = {{1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
+    static int minStep = 0;
 
+    static class Position {
+        int level;
+        int row;
+        int column;
+        int step;
+
+        public Position(int l, int r, int c, int s) {
+            row = r;
+            level = l;
+            column = c;
+            step = s;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
-            int levels = scanner.nextInt();
-            int rows = scanner.nextInt();
-            int columns = scanner.nextInt();
-            if (levels == 0 && rows == 0 && columns == 0) {
+            int l = scanner.nextInt();
+            int r = scanner.nextInt();
+            int c = scanner.nextInt();
+            if (l == 0 && r == 0 && c == 0) {
                 return;
             }
-            dungeon = new char[levels][rows][columns];
-            vis = new boolean[levels][rows][columns];
-            for (int l = 0; l < levels; l++) {
-                for (int r = 0; r < rows; r++) {
-                    dungeon[l][r] = scanner.next().toCharArray();
-                    for (int c = 0; c < dungeon[l][r].length; c++) {
-                        char cr = dungeon[l][r][c];
-                        if (cr == 'S') {
-                            start = new MyPoint(l, r, c, 0);
+            minStep = 0;
+            char[][][] dungeon = new char[l][r][c];
+            boolean[][][] visit = new boolean[l][r][c];
+            int startR = 0, startC = 0, startL = 0;
+            for (int i = 0; i < l; i++) {
+                for (int j = 0; j < r; j++) {
+                    char[] letters = scanner.next().toCharArray();
+                    for (int k = 0; k < letters.length; k++) {
+                        dungeon[i][j][k] = letters[k];
+                        if (Objects.equals(letters[k], "S")) {
+                            startL = i;
+                            startR = j;
+                            startC = k;
                         }
-                        if (cr == 'E') {
-                            end = new MyPoint(l, r, c, 0);
-                        }
+
                     }
                 }
             }
-            result = -1;
-            bfs(levels, rows, columns);
-            if (result > -1) {
-                System.out.println("Escaped in " + result + " minute(s).");
-            } else {
+            bsf(dungeon, visit, startL, startR, startC);
+            if (minStep == 0) {
                 System.out.println("Trapped!");
+            } else {
+                System.out.println("Escaped in " + minStep + " minute(s).");
             }
-
         }
-
     }
 
-    static void bfs(int levels, int rows, int colums) {
-        LinkedList<MyPoint> queue = new LinkedList();
-        queue.add(start);
-        vis[start.level][start.row][start.column] = true;
-        while (!queue.isEmpty()) {
-            MyPoint p = queue.poll();
-            if (p.level == end.level && p.row == end.row
-                    && p.column == end.column) {
-                result = p.step;
+    private static void bsf(char[][][] dungeon, boolean[][][] visit, int startL, int startR, int startC) {
+        Position[] queue = new Position[100000];
+        int head = 0;
+        int tail = 0;
+        queue[tail++] = new Position(startL, startR, startC, 0);
+        while (head < tail) {
+            Position p = queue[head++];
+            if (dungeon[p.level][p.row][p.column]==('E')) {
+                minStep = p.step;
                 return;
             }
-            for (int i = 0; i < dirs.length; i++) {
-                MyPoint next = new MyPoint(p.level + dirs[i][0],
-                        p.row + dirs[i][1], p.column + dirs[i][2], p.step + 1);
-                if (check(next, levels, rows, colums)) {
-                    queue.offer(next);
-                    vis[next.level][next.row][next.column] = true;
-
-
+            for (int i = 0; i < moves.length; i++) {
+                int[] move = moves[i];
+                Position next = new Position(p.level + move[0], p.row + move[1], p.column + move[2], p.step + 1);
+                if (check(next, dungeon, visit)) {
+                    queue[tail++] = next;
+                    visit[next.level][next.row][next.column] = true;
                 }
             }
-
         }
     }
 
-    static boolean check(MyPoint p, int level, int row, int column) {
+    private static boolean check(Position position, char[][][] dungeon, boolean[][][] visit) {
+        //检查边界
+        if (position.level >= dungeon.length || position.level < 0 || position.row >= dungeon[0].length || position.row < 0 || position.column >= dungeon[0][0].length || position.column < 0) {
+            return false;
+        }
+        //是否是石头
+        if (dungeon[position.level][position.row][position.column]==('#')) {
+            return false;
+        }
+        //已经访问过
+        if (visit[position.level][position.row][position.column]) return false;
 
-        if (p.level < 0 || p.level >= level) {
-            return false;
-        }
-        if (p.row < 0 || p.row >= row) {
-            return false;
-        }
-        if (p.column < 0 || p.column >= column) {
-            return false;
-        }
-        if (vis[p.level][p.row][p.column]) {
-            return false;
-        }
-        if (dungeon[p.level][p.row][p.column] == '#') {
-            return false;
-        }
+
         return true;
     }
-
-
-}
-
-class MyPoint {
-    int level;
-    int row;
-    int column;
-    int step;
-
-    public MyPoint(int l, int r, int c, int s) {
-        level = l;
-        row = r;
-        column = c;
-        step = s;
-    }
-
 }
